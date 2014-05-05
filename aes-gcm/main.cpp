162c07bb4375211aaadd0c9e7580ee2edbdd6012
Michael Clark <michael@metaparadigm.com>
2014-05-05 14:50:06 +0800

    Initial Commit

---
+++ aes-gcm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  aes-gcm
+//
+//  Created by Michael Clark on 5/5/14.
+//  Copyright (c) 2014 Metaparadigm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
